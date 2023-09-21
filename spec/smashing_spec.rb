54c237f09b53bbb032539033238d6aadd13c9a74
Oleksii <92565255+kova1chuk@users.noreply.github.com>
2023-09-21 12:29:09 +0300

    Update smashing_spec.rb

--- spec/smashing_spec.rb
+++ spec/smashing_spec.rb
@@ -15,26 +15,28 @@ describe 'Smashing Wallpaper Downloader' do
   end
 
   describe 'fetch_wallpaper_links' do
-    before do
-      stub_request(:get, base_url)
-        .to_return(body: <<-HTML
-          <a href="wallpaper1.jpg">Wallpaper 1</a>
-          <a href="wallpaper2.png">Wallpaper 2</a>
-        HTML
-                  )
+    context 'when fetching links successfully' do
+      before do
+        stub_request(:get, base_url)
+          .to_return(body: <<-HTML
+            <a href="wallpaper1.jpg">Wallpaper 1</a>
+            <a href="wallpaper2.png">Wallpaper 2</a>
+          HTML
+                    )
+      end
 
-      Dir.mkdir('2023-01_640x480_wallpapers') unless Dir.exist?('2023-01_640x480_wallpapers')
+      it 'returns an array of wallpaper links' do
+        links = fetch_wallpaper_links(base_url)
+        expect(links).to include('wallpaper1.jpg', 'wallpaper2.png')
+      end
     end
 
-    it 'returns an array of wallpaper links' do
-      links = fetch_wallpaper_links(base_url)
-      expect(links).to include('wallpaper1.jpg', 'wallpaper2.png')
-    end
+    context 'when handling errors' do
+      it 'handles errors gracefully' do
+        stub_request(:get, base_url).to_raise(OpenURI::HTTPError.new('404 Not Found', nil))
 
-    it 'handles errors gracefully' do
-      stub_request(:get, base_url).to_raise(OpenURI::HTTPError.new('404 Not Found', nil))
-
-      expect { fetch_wallpaper_links(base_url) }.to output(/Error fetching or parsing the page/).to_stdout
+        expect { fetch_wallpaper_links(base_url) }.to output(/Error fetching or parsing the page/).to_stdout
+      end
     end
   end
 
@@ -51,31 +53,37 @@ describe 'Smashing Wallpaper Downloader' do
       File.delete(wallpaper_path) if File.exist?(wallpaper_path)
     end
 
-    it 'downloads a wallpaper matching the resolution' do
-      stub_request(:get, File.join(base_url, url))
-        .to_return(body: 'Downloaded: mar-22-spring-is-coming-cal-640x480.jpg')
+    context 'when downloading successfully' do
+      it 'downloads a wallpaper matching the resolution' do
+        stub_request(:get, File.join(base_url, url))
+          .to_return(body: 'Downloaded: mar-22-spring-is-coming-cal-640x480.jpg')
 
-      allow(Down).to receive(:download).and_return(File.new(wallpaper_path, 'w'))
+        allow(Down).to receive(:download).and_return(File.new(wallpaper_path, 'w'))
 
-      download_wallpaper(base_url, url, resolution, download_directory)
-      expect(File.exist?(wallpaper_path)).to be_truthy
+        download_wallpaper(base_url, url, resolution, download_directory)
+        expect(File.exist?(wallpaper_path)).to be_truthy
+      end
     end
 
-    it 'skips downloading if the resolution does not match' do
-      stub_request(:get, File.join(base_url, url))
-        .to_return(body: 'Sample Wallpaper Content')
+    context 'when resolution does not match' do
+      it 'skips downloading' do
+        stub_request(:get, File.join(base_url, url))
+          .to_return(body: 'Sample Wallpaper Content')
 
-      download_wallpaper(base_url, url, '800x600', download_directory)
+        download_wallpaper(base_url, url, '800x600', download_directory)
 
-      wallpaper_path = File.join(download_directory, url)
-      expect(File).not_to exist(wallpaper_path)
+        wallpaper_path = File.join(download_directory, url)
+        expect(File).not_to exist(wallpaper_path)
+      end
     end
 
-    it 'handles errors gracefully' do
-      stub_request(:get, File.join(base_url, url)).to_raise(StandardError)
+    context 'when handling errors' do
+      it 'handles errors gracefully' do
+        stub_request(:get, File.join(base_url, url)).to_raise(StandardError)
 
-      expect { download_wallpaper(base_url, url, resolution, download_directory) }
-        .to output(/Failed to download/).to_stdout
+        expect { download_wallpaper(base_url, url, resolution, download_directory) }
+          .to output(/Failed to download/).to_stdout
+      end
     end
   end
 
